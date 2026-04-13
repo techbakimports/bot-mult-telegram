56d74abda311db78ffa0bdd424d2d0c2ae71d00f
techbakimports <techbak.imports@gmail.com>
2026-04-12 21:10:24 -0400

    Delete dead feature module converters.py

--- converters.py
+++
@@ -1,8 +0,0 @@
-"""
-Funcoes de conversao
-"""
-import requests
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import ContextTypes
-from utils import is_authorized
-
